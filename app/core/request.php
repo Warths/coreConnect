031755eeb441fc67be7e0ffad59a4e4d1d5d1b93
Warths <pro.warths@gmail.com>
2022-05-22 22:42:28 +0200

    Added log related functions

--- app/core/request.php
+++ app/core/request.php
@@ -3,7 +3,30 @@
 namespace App\Core;
 
 class Request {
-    public function serveTime() {
+    private $logger;
+
+    public function __construct() {
+        $this->logger = new Logger($this->clientName());
+        $this->logger->discrete("Handling Request");
+        
+    }
+
+    public function __destruct() {
+        $this->logger->discrete(
+          "Served in ".$this->serveTime(). "ms."
+        );
+    }
+
+    public function get($key) {
+        var_dump($_GET);
+    } 
+
+    public function clientName() {
+        return $_SERVER["REMOTE_ADDR"] . ":" . $_SERVER["REMOTE_PORT"];
+    }
+
+    private function serveTime() {
         return intval(microtime(true)*1000-$_SERVER["REQUEST_TIME_FLOAT"]*1000);
     }
+
 }
