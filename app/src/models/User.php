f6c7f898db501d808c7a87283c3e4fcb8b5c45f5
L <pro.warths@gmail.com>
2022-06-25 11:43:54 +0200

    Made userRepository

--- app/src/models/User.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace App;
-
-use App\Core\Model; 
---
+++ app/src/models/UserRepository.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Src\Models;
+
+use App\Core\Model; 
+
+class UserRepository extends Model {
+    protected $table = "user";
+}
