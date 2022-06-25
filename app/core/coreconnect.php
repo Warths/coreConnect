986bfbc8fd4c15376679650b43e19250dba022c6
L <pro.warths@gmail.com>
2022-06-25 10:05:54 +0200

    Added controller autoload

--- app/core/coreconnect.php
+++ app/core/coreconnect.php
@@ -5,6 +5,7 @@ namespace App\Core;
 use App\Core\Environment;
 use App\Core\Logger;
 use App\Core\Request;
+use App\Core\Routing;
 
 class CoreConnect {
     private $env;
@@ -12,7 +13,9 @@ class CoreConnect {
     function __construct() {
         // Setting env up
         $this->env = new Environment();
+        $this->env->load();
 
+        $this->loadControllers();
         // Configuring logging.
         $this->logger = new Logger("Core");
         
@@ -20,7 +23,17 @@ class CoreConnect {
 
     }
 
-    function serve() {
+    private function loadControllers() {
+        $ctrl_path = __DIR__."/../src/controllers/";
+        $controllers = scandir($ctrl_path);
+        foreach($controllers as $controller) {
+            if (is_file($ctrl_path.$controller)) {
+                require $ctrl_path.$controller;
+            }
+        }
+    }
+
+    public function serve() {
         // STARTING SERVING CLIENT
         $request = new Request();
         // END SERVING CLIENT 
