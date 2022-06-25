30f322630fe0c3ebafd7ad98a101b7698bcf28c4
L <pro.warths@gmail.com>
2022-06-25 17:36:13 +0200

    Added queries to repo

--- app/src/models/UserRepository.php
+++ app/src/models/UserRepository.php
@@ -4,5 +4,29 @@ namespace App\Src\Models;
 use App\Core\Model; 
 
 class UserRepository extends Model {
-    protected $table = "user";
+    protected $table = "users";
+
+    function getUserById($id) {
+        return $this->fetch("SELECT id, name, email from $this->table WHERE id = ?", [$id]);
+    }
+
+    function getUserByLogin($login) {
+        return $this->fetch("SELECT * from $this->table WHERE name = ? OR email = ?;", [$login, $login]);
+    }
+
+
+    function createUser($name, $email, $password) {
+        $timestamp = time();
+        $salted_pass = $timestamp.$password;
+
+
+
+        return $this->query(
+            "INSERT into $this->table 
+                (id, name, pass, email, registered_date, permissions_level, banned, activation_key)
+            VALUES
+                (NULL, ?, MD5(?), ?, ?, 0, 0, MD5(RAND()));",
+            [$name, $salted_pass, $email, $timestamp]
+        );
+    }
 }
