bb7da22b39db65cae4ddcc999652af7ef79f6ed7
L <pro.warths@gmail.com>
2022-06-25 10:06:12 +0200

    Added DB abstraction

--- app/core/database.php
+++ app/core/database.php
@@ -1,13 +1,12 @@
 <?php
 
-namespace App\Database;
-
-//require_once __DIR__ . "../../../env.php";
-
+namespace App\Core;
 
 class Database {
-    function __construct()
-    {
-
+    protected $db;
+    function __construct() {
+        $host = $_ENV["DB_HOST"]; $name = $_ENV["DB_NAME"];
+        $user = $_ENV["DB_USER"]; $pass = $_ENV["DB_PASS"];
+        $this->db = new \PDO("mysql:host=$host;dbname=$name", $user, $pass);
     }
 }
