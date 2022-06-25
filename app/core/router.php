c62a9e066197175e546d97621a0c655018bd75fe
L <pro.warths@gmail.com>
2022-06-25 11:20:40 +0200

    Added router default response

--- app/core/router.php
+++ app/core/router.php
@@ -3,23 +3,37 @@
 namespace App\Core;
 
 use App\Core\Route;
+use App\Core\Response\HttpResponse;
+use App\Core\Response\JsonResponse;
+use App\Core\Response\Generics;
 
 class Router {
     static $instance = null;
     private $routes = [];
 
     public function serve() {
+        $rest_only = isset($_ENV["REST_ONLY"]) && $_ENV["REST_ONLY"] == "1";
+
         foreach($this->routes as $route) {
             $params = $route->match();
             if ($params) {
-                // SERVE
-                call_user_func($route->callable, [$params]);
-            }
-            
-            if (isset($_ENV["REST_ONLY"]) && $_ENV["REST_ONLY"] == "1") {
+                $response = call_user_func($route->callable, [$params]);
+                if (is_a($response, HttpResponse::class)) {
+                    $response->render();
+                    return;
+                }
                 
-            }
-        } 
+                // No Http Response ?
+                $rest_only ?
+                    (new JsonResponse(...Generics::json500))->render(): 
+                    (new HttpResponse(...Generics::http500))->render();
+                return;
+            } 
+        }
+
+        $rest_only ?
+            (new JsonResponse(...Generics::json404))->render(): 
+            (new HttpResponse(...Generics::http404))->render();
     }
 
     public function addRoute($pattern, $callable) {
