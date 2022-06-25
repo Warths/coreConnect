9a45a9479ad216e5641bc7d5d2313414d6e8e8c4
L <pro.warths@gmail.com>
2022-06-25 18:54:45 +0200

    Completed authentication and validation

--- app/src/controllers/user.php
+++ app/src/controllers/user.php
@@ -6,30 +6,38 @@ use App\Core\Response\JsonResponse;
 use App\Src\Models\UserRepository;
 
 class UserController extends Controller {
+
+
     function create_user() {
         $repo = new UserRepository();
         // TODO VALIDATION
         $repo->createUser($_POST["name"], $_POST["email"], $_POST["password"]);
         return new JsonResponse($_POST);
     }
-
-    function check_pass($user, $pass) {
-        return md5($user["registered_date"].$pass) == $user["pass"];
-    }
-
+    
     function authenticate() {
         $repo = new UserRepository();
         $user = $repo->getUserByLogin($_POST["login"]);
-        $authenticated = $this->check_pass($user, $_POST["pass"]);
+        $authenticated = md5($user["registered_date"].$_POST["pass"]) == $user["pass"];
+        if ($authenticated) {
+            $token = JWT::encode(["username"=>$user["name"]]);
+            return new JsonResponse(["token"=>$token]);
+        } 
+        return new JsonResponse(["error"=>"Unauthorized"], 401);
         
     }
 
-
-    function testjwt() {
-        JWT::encodeJWT(["payload"]);
+    function validate() {
+        $authorization = getallheaders()["Authorization"];
+        $token = new JWT($authorization);
+        if ($token->validate()) {
+            return new JsonResponse($token->get_payload());
+        }
+        return new JsonResponse(["error"=>"Unauthorized"], 401);
     }
-    
 }
 
 UserController::describe("/user/create/", "create_user", ["POST"]);
-UserController::describe("/user/authenticate/", "testjwt", ["POST"]);
+UserController::describe("/user/authenticate/", "authenticate", ["POST"]);
+UserController::describe("/user/validate/", "validate", ["POST"]);
+
