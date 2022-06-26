b03a2e44308dcbb569f11e037e6c2eb3c84508bd
L <pro.warths@gmail.com>
2022-06-26 12:26:23 +0200

    Made add_command method

--- app/src/models/CommandRepository.php
+++ app/src/models/CommandRepository.php
@@ -6,7 +6,16 @@ use App\Core\Model;
 class CommandRepository extends Model {
     protected $table = "commands";
 
-    function getByCommand($command) {
+    function get_by_command($command) {
         return $this->fetch("SELECT * FROM $this->table WHERE command = ?", [$command]);
+    }
+
+    function add_command($command, $description, $heading, $cooldown) {
+        return $this->query("INSERT INTO $this->table 
+                                 (id, command, description, heading, cooldown)
+                             VALUES
+                                 (NULL, ?, ?, ?, ?)",
+                             [$command, $description, $heading, $cooldown]
+    );
     }
 }
